3a7e3c91f6c4136d8ff3eedee62eb02ba2c02f99
taomaokun@MacBookPro <taomaokun@foxmail.com>
2016-08-16 10:23:45 +0800

    增加经典的singleton模式

--- src/main/java/com/java/designpattern/singleton/classic/Singleton.java
+++ src/main/java/com/java/designpattern/singleton/classic/Singleton.java
@@ -2,6 +2,7 @@ package com.java.designpattern.singleton.classic;
 
 /**
  * <pre>使用经典模式创建一个单例</pre>
+ * <blockquote>延迟加载／懒加载(lazy initialize)</blockquote>
  * <blockquote>非线程安全</blockquote>
  * @author taomk 2016年8月15日 下午10:29:52
  */
