2562dddbe7f8c9df18fe3d4e09ad1a79415e65a1
taomaokun@MacBookPro <taomaokun@foxmail.com>
2016-08-24 15:49:18 +0800

    JFrame中的模版方法模式示例

---
+++ src/main/java/com/java/designpattern/templatemethod/jframe/MyFrame.java
@@ -0,0 +1,34 @@
+package com.java.designpattern.templatemethod.jframe;
+
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+
+/**
+ * JFrame中的模版方法模式
+ * @author taomk
+ * 2016年8月24日 下午3:46:56
+ */
+public class MyFrame extends JFrame {
+
+	private static final long serialVersionUID = -3581937486249372724L;
+	
+	public MyFrame(String title) {
+		super(title);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		this.setSize(300,300);
+		this.setVisible(true);
+	}
+	
+	public void paint(Graphics graphics) {
+		super.paint(graphics);
+		String msg = "I rule!!";
+		graphics.drawString(msg, 100, 100);
+	}
+
+	public static void main(String[] args) {
+		@SuppressWarnings("unused")
+		MyFrame myFrame = new MyFrame("Head First Design Patterns");
+	}
+}
