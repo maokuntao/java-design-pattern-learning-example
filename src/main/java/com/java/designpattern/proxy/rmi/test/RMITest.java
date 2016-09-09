107d43c6b9489090cab123b753b471dfedc27430
taomk <taomk@yonyou.com>
2016-09-09 10:47:44 +0800

    增加代理模式-rmi说明参考地址

--- src/main/java/com/java/designpattern/proxy/rmi/test/RMITest.java
+++ src/main/java/com/java/designpattern/proxy/rmi/test/RMITest.java
@@ -4,7 +4,9 @@ import com.java.designpattern.proxy.rmi.client.MyRemoteClient;
 import com.java.designpattern.proxy.rmi.server.MyRemoteImpl;
 
 /**
- * 测试类
+ * 测试类<br>
+ * <p><b>RMI</b> &nbsp;  参考地址 : <a>http://blog.csdn.net/a19881029/article/details/9465663</a></p>
+ * 	
  *
  * @author taomk
  * 2016年9月9日 上午10:32:37
