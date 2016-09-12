389874a3665ce1093e281612912bcdbf08f3b563
taomaokun@MacBookPro <taomaokun@foxmail.com>
2016-09-12 21:34:04 +0800

    调整格式

--- src/main/java/com/java/designpattern/proxy/rmi/virtualproxy/test/ImageProxyTest.java
+++ src/main/java/com/java/designpattern/proxy/rmi/virtualproxy/test/ImageProxyTest.java
@@ -15,67 +15,66 @@ import com.java.designpattern.proxy.rmi.virtualproxy.ImageProxy;
 /**
  * 测试类
  *
- * @author taomk
- * 2016年9月12日 下午8:59:28
+ * @author taomk 2016年9月12日 下午8:59:28
  */
 public class ImageProxyTest {
 
 	ImageComponent imageComponent;
 	JFrame frame = new JFrame("CD Cover Viewer");
-    JMenuBar menuBar;
-    JMenu menu;
-	Hashtable<String , String> cds = new Hashtable<String , String>();
- 
-	public static void main (String[] args) throws Exception {
+	JMenuBar menuBar;
+	JMenu menu;
+	Hashtable<String, String> cds = new Hashtable<String, String>();
+
+	public static void main(String[] args) throws Exception {
 		new ImageProxyTest();
 	}
- 
-	public ImageProxyTest() throws Exception{
-		
+
+	public ImageProxyTest() throws Exception {
+
 		menuBar = new JMenuBar();
 		menu = new JMenu("Favorite CDs");
-        menuBar.add(menu);
-        frame.setJMenuBar(menuBar);
-        
-		cds.put("Ambient: Music for Airports","http://images.amazon.com/images/P/B000003S2K.01.LZZZZZZZ.jpg");
-        cds.put("Buddha Bar","http://images.amazon.com/images/P/B00009XBYK.01.LZZZZZZZ.jpg");
-        cds.put("Ima","http://images.amazon.com/images/P/B000005IRM.01.LZZZZZZZ.jpg");
-        cds.put("Karma","http://images.amazon.com/images/P/B000005DCB.01.LZZZZZZZ.gif");
-        cds.put("MCMXC A.D.","http://images.amazon.com/images/P/B000002URV.01.LZZZZZZZ.jpg");
-        cds.put("Northern Exposure","http://images.amazon.com/images/P/B000003SFN.01.LZZZZZZZ.jpg");
-        cds.put("Selected Ambient Works, Vol. 2","http://images.amazon.com/images/P/B000002MNZ.01.LZZZZZZZ.jpg");
+		menuBar.add(menu);
+		frame.setJMenuBar(menuBar);
 
-		URL initialURL = new URL((String)cds.get("Selected Ambient Works, Vol. 2"));
-		for(Enumeration<String> e = cds.keys(); e.hasMoreElements();) {
-			String name = (String)e.nextElement();
-        	JMenuItem menuItem = new JMenuItem(name);
-        	menu.add(menuItem); 
-        	menuItem.addActionListener(new ActionListener() {
-          		  public void actionPerformed(ActionEvent event) {
-           		     imageComponent.setIcon(new ImageProxy(getCDUrl(event.getActionCommand())));
+		cds.put("Ambient: Music for Airports", "http://images.amazon.com/images/P/B000003S2K.01.LZZZZZZZ.jpg");
+		cds.put("Buddha Bar", "http://images.amazon.com/images/P/B00009XBYK.01.LZZZZZZZ.jpg");
+		cds.put("Ima", "http://images.amazon.com/images/P/B000005IRM.01.LZZZZZZZ.jpg");
+		cds.put("Karma", "http://images.amazon.com/images/P/B000005DCB.01.LZZZZZZZ.gif");
+		cds.put("MCMXC A.D.", "http://images.amazon.com/images/P/B000002URV.01.LZZZZZZZ.jpg");
+		cds.put("Northern Exposure", "http://images.amazon.com/images/P/B000003SFN.01.LZZZZZZZ.jpg");
+		cds.put("Selected Ambient Works, Vol. 2", "http://images.amazon.com/images/P/B000002MNZ.01.LZZZZZZZ.jpg");
+
+		URL initialURL = new URL((String) cds.get("Selected Ambient Works, Vol. 2"));
+		for (Enumeration<String> e = cds.keys(); e.hasMoreElements();) {
+			String name = (String) e.nextElement();
+			JMenuItem menuItem = new JMenuItem(name);
+			menu.add(menuItem);
+			menuItem.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent event) {
+					imageComponent.setIcon(new ImageProxy(getCDUrl(event.getActionCommand())));
 					frame.repaint();
-           	      }
-        	});
+				}
+			});
 		}
- 		
+
 		// set up frame and menus
- 
+
 		Icon icon = new ImageProxy(initialURL);
 		imageComponent = new ImageComponent(icon);
 		frame.getContentPane().add(imageComponent);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(800,600);
+		frame.setSize(800, 600);
 		frame.setVisible(true);
 
 	}
 
 	URL getCDUrl(String name) {
 		try {
-			return new URL((String)cds.get(name));
+			return new URL((String) cds.get(name));
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
-	
+
 }
