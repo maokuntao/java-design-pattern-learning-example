5fb286379260d745d1df703b2d61c33d5afdd234
taomaokun@MacBookPro <taomaokun@foxmail.com>
2016-09-19 16:00:24 +0800

    增加复合模式－装饰者模式示例

---
+++ src/main/java/com/java/designpattern/compound/decorator/QuackCounter.java
@@ -0,0 +1,31 @@
+package com.java.designpattern.compound.decorator;
+
+/**
+ * 使用装饰者模式给呱呱叫这一行为添加技术功能
+ *
+ * @author taomk
+ * 2016年9月19日 下午3:55:01
+ */
+public class QuackCounter implements Quackable{
+
+	private Quackable duck;
+	private static int numberOfQuack;
+	
+	public QuackCounter(Quackable duck){
+		this.duck = duck;
+	}
+
+	@Override
+	public void quack() {
+		duck.quack();
+		numberOfQuack++;
+	}
+	
+	/**
+	 * 呱呱叫的次数
+	 */
+	public static int getQuacks(){
+		return numberOfQuack;
+	}
+	
+}
