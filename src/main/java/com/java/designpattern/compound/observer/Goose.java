594094d78c09f5028389db0b42b17522f19f5fb1
taomaokun@MacBookPro <taomaokun@foxmail.com>
2016-09-20 19:52:24 +0800

    新增鹅适配器

---
+++ src/main/java/com/java/designpattern/compound/observer/Goose.java
@@ -0,0 +1,18 @@
+package com.java.designpattern.compound.observer;
+
+/**
+ * 鹅
+ *
+ * @author taomk
+ * 2016年9月20日 下午7:51:32
+ */
+public class Goose {
+
+	public void honk() {
+		System.out.println("Honk");
+	}
+
+	public String toString() {
+		return "Goose";
+	}
+}
