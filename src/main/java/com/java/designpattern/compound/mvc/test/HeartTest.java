aa368cf0386dc5ceaaa51410173ab2fc2dceaefd
taomaokun@MacBookPro <taomaokun@foxmail.com>
2016-09-21 20:15:17 +0800

    修正测试类名称

--- src/main/java/com/java/designpattern/compound/mvc/test/HeartTest.java
+++ src/main/java/com/java/designpattern/compound/mvc/test/HeartTest.java
@@ -6,7 +6,7 @@ import com.java.designpattern.compound.mvc.*;
  * @author taomk
  * 2016年9月21日 下午8:01:08
  */  
-public class HeartTestDrive {
+public class HeartTest {
 
     @SuppressWarnings("unused")
 	public static void main (String[] args) {
