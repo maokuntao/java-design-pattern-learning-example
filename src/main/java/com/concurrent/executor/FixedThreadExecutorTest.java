ed19ba38fad0d88cbbb3f038d4170e06d446cd2d
taomaokun@MacBookPro <taomaokun@foxmail.com>
2017-04-27 15:25:38 +0800

    add fixedThreadExecutor

---
+++ src/main/java/com/concurrent/executor/FixedThreadExecutorTest.java
@@ -0,0 +1,42 @@
+package com.concurrent.executor;
+
+import java.util.concurrent.Executors;
+
+/**
+ * <pre>
+ * 测试: {@link java.util.concurrent.Executors.newFixedThreadPool()}
+ * 
+ * 返回的结果表明newFixedThreadPool创建一个定长线程池，可控制线程最大并发数，超出的线程会在队列中等待。
+ * 
+ * 数量设置一般和Runtime.getRuntime().availableProcessors();一致
+ * </pre>
+ * @author taomk 2017年4月27日 下午3:10:30
+ *
+ */
+public class FixedThreadExecutorTest {
+
+	public static void main(String[] args) {
+
+		java.util.concurrent.ExecutorService fixedExecutorService = Executors.newFixedThreadPool(3);
+
+		
+		for (int i = 0; i < 10; i++) {
+			
+			fixedExecutorService.execute(new Runnable() {
+				
+				@Override
+				public void run() {
+
+					System.out.println(Thread.currentThread().getName() + " is running... ");
+					try {
+						Thread.sleep(500);
+					} catch (InterruptedException e) {
+					}
+				}
+			});
+		}
+		
+		System.out.println("Outter:" + Thread.currentThread().getName() + " is running... ");
+	}
+
+}
