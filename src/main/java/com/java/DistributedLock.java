599c86d4dba8e866bbb524143bdcec93c9a1e892
taomaokun@MacBookPro <taomaokun@foxmail.com>
2017-10-09 15:12:31 +0800

    add comment

--- src/main/java/com/java/DistributedLock.java
+++ src/main/java/com/java/DistributedLock.java
@@ -18,11 +18,21 @@ import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.data.Stat;
 
 /**
- * DistributedLock lock = null; try { lock = new
- * DistributedLock("127.0.0.1:2182","test"); lock.lock(); //do something... }
- * catch (Exception e) { e.printStackTrace(); } finally { if(lock != null)
- * lock.unlock(); }
- * 
+ * <pre>
+ * <code>
+ * DistributedLock lock = null; 
+ * try { lock = new
+ * 	DistributedLock("127.0.0.1:2182","test"); 
+ * 	lock.lock(); 
+ * 	//do something... 
+ * } catch (Exception e) {
+ *  	e.printStackTrace(); 
+ * } finally { 
+ *  if(lock != null)
+ * 	lock.unlock(); 
+ * }
+ * </code>
+ * </pre>
  * @author taomk
  *
  */
